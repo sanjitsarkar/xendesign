0744fe1beb89b8600e672ff647f75d0ac4e9800f
Sanjit <ssanjit856@gmail.com>
2022-02-12 21:38:41 +0530

    refactored index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
 //Method to close Alert
-function closeAlert(elem){
-    elem.parentNode.classList.add('alert-hide');
-    setTimeout(()=>{
-        elem.parentNode.style.display = 'none';
-    elem.parentNode.classList.remove('alert-hide');
-    
-    
-    },300)
-    }
+function closeAlert(elem) {
+  elem.parentNode.classList.add("alert-hide");
+  setTimeout(() => {
+    elem.parentNode.style.display = "none";
+    elem.parentNode.classList.remove("alert-hide");
+  }, 300);
+}
 //Slider
 const slider = document.querySelector("#slider");
 const output = document.querySelector("#slider-value");
-output.textContent = slider.value; 
+output.textContent = slider.value;
 
-slider.oninput = function() {
+slider.oninput = function () {
   output.textContent = this.value;
-}
+};
