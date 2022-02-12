a701ac141062287ea662a3508cee5aebc150c5da
Sanjit <ssanjit856@gmail.com>
2022-02-12 16:28:53 +0530

    fixed modal

--- js/index.js
+++ js/index.js
@@ -16,3 +16,32 @@ output.textContent = slider.value;
 slider.oninput = function() {
   output.textContent = this.value;
 }
+
+//Modal
+const modal = document.querySelector("#modal");
+
+var openModal = document.querySelector("#open-modal");
+
+var close = document.querySelector("#close");
+
+openModal.onclick = function() {
+//   modal.style.display = "block";
+  modal.classList.add('modal-open')
+
+}
+
+close.onclick = function() {
+//   modal.style.display = "none";
+  modal.classList.remove('modal-open')
+
+
+ 
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    // modal.style.display = "none";
+  modal.classList.remove('modal-open')
+
+  }
+}
