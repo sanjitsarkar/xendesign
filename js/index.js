b8b13cf54497328396ddf669aa067dcd02fa5eb0
Sanjit Sarkar <ssanjit856@gmail.com>
2022-02-13 15:43:56 +0530

    modfied insex.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,73 @@
-//Method to close Alert
+//Slider
+const slider = document.querySelector("#slider");
+const output = document.querySelector("#slider-value");
+output.textContent = slider.value;
+
+slider.oninput = function () {
+    output.textContent = this.value;
+}
+
+//Modal
+const modal = document.querySelector("#modal");
+
+var openModal = document.querySelector("#open-modal");
+
+var close = document.querySelector("#close");
+
+openModal.onclick = function () {
+    modal.classList.add('modal-open')
+}
+close.onclick = function () {
+    modal.classList.remove('modal-open')
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.classList.remove('modal-open')
+    }
+}
+
+//Rating
+const rating = document.querySelectorAll('.rating');
+const clearRating = (n) => {
+    for (var i = n + 1; i < 5; i++) {
+        rating[i].classList.remove('rating-active');
+    }
+}
+const addRating = (n) => {
+    clearRating(n);
+    for (var i = 0; i <= n; i++) {
+        rating[i].classList.add('rating-active')
+    }
+}
+
+
+
+rating[0].addEventListener("click", () => {
+    addRating(0);
+})
+rating[1].addEventListener("click", () => {
+    addRating(1);
+})
+rating[2].addEventListener("click", () => {
+    addRating(2);
+})
+rating[3].addEventListener("click", () => {
+    addRating(3);
+})
+rating[4].addEventListener("click", () => {
+    addRating(4);
+})
+
+//Toast
+const showToast = (container, duration = 1000) => {
+    const toast = document.querySelector(`#${container}`);
+    toast.classList.add('toast-open');
+    setTimeout(() => {
+        toast.classList.remove('toast-open');
+    },duration)
+}
+
 function closeAlert(elem) {
     elem.parentNode.classList.add('alert-hide');
     setTimeout(() => {
