3c859c9599d13fcb7f8bfd5f9d93236c3a2b5071
Sanjit <ssanjit856@gmail.com>
2022-02-14 22:26:43 +0530

    added input field with icon

--- js/index.js
+++ js/index.js
@@ -1,26 +1,4 @@
 //Method to close Alert
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-function closeAlert(elem){
-=======
-function closeAlert(elem) {
->>>>>>> feature/button
-    elem.parentNode.classList.add('alert-hide');
-    setTimeout(() => {
-        elem.parentNode.style.display = 'none';
-<<<<<<< HEAD
-    elem.parentNode.classList.remove('alert-hide');
-    
-    
-    },300)
-    }
-=======
-=======
->>>>>>> feature/card
->>>>>>> feature/image
 function closeAlert(elem) {
   elem.parentNode.classList.add("alert-hide");
   setTimeout(() => {
@@ -28,24 +6,11 @@ function closeAlert(elem) {
     elem.parentNode.classList.remove("alert-hide");
   }, 300);
 }
-<<<<<<< HEAD
 //Slider
 const slider = document.querySelector("#slider");
 const output = document.querySelector("#slider-value");
 output.textContent = slider.value;
-
 slider.oninput = function () {
   output.textContent = this.value;
 };
 
-=======
-<<<<<<< HEAD
->>>>>>> feature/button
-=======
-        elem.parentNode.classList.remove('alert-hide');
-    }, 300)
-}
->>>>>>> feature/button
-=======
->>>>>>> feature/card
->>>>>>> feature/image
