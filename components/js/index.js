2137bca2f97b93a8290e26e5e53c91579cba5f7d
Sanjit <ssanjit856@gmail.com>
2022-02-17 01:40:22 +0530

    added mobile responsiveness

--- components/js/index.js
+++ components/js/index.js
@@ -1,4 +1,18 @@
 const page = document.querySelector('#iframe');
-const openPage = (pageName)=>{
-    page.src=`${pageName.textContent.toLowerCase()}.html`;
+const topList = document.querySelector('ul#top-list');
+const bottomList = document.querySelector('ul#bottom-list');
+const openPage = (e)=>{
+    let page = e.target.textContent.toLowerCase()
+    if(page==="installation")
+        page="index";
+    window.location.href=`${page}.html`;
 }
+
+topList.addEventListener("click",openPage);
+bottomList.addEventListener("click",openPage);
+
+const menu = document.querySelector("button.menu");
+const sideBar = document.querySelector("aside.sidebar");
+menu.addEventListener("click",()=>{
+sideBar.classList.toggle("show");
+})
